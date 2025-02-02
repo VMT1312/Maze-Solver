5411e9046bee8a63cdd5f6579ec7d7c577c4b50c
VMT1312 <thiemmavinh123@gmail.com>
2025-02-02 11:49:18 +0800

    Test draw line

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from window import Window
+from window import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(
+        Line(
+            Point(1, 1),
+            Point(100, 8)
+        ),
+        'red'
+    )
     win.wait_for_close()
 
 
