bf260caebd7b918eadd5b37c192ba93add211514
VMT1312 <thiemmavinh123@gmail.com>
2025-02-02 12:27:30 +0800

    Draw move method between 2 cells

--- cell.py
+++ cell.py
@@ -51,3 +51,20 @@ class Cell:
                     Point(self.__x2, self.__y2)
                 )
             )
+
+    def draw_move(self, to_cell, undo=False):
+        line = Line(
+                Point(
+                    (self.__x1 + self.__x2) / 2,
+                    (self.__y1 + self.__y2) / 2
+                ),
+                Point(
+                    (to_cell.__x1 + to_cell.__x2) / 2,
+                    (to_cell.__y1 + to_cell.__y2) / 2
+                )
+            )
+        
+        if undo:
+            line.draw(self.__win.__canvas, 'red')
+        else:
+            line.draw(self.__win.__canvas, 'gray')
