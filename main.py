ac54a8a6d22f0cf75e03551c1ff4bc66af75e034
VMT1312 <thiemmavinh123@gmail.com>
2025-02-02 13:01:19 +0800

    Draw maze

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from window import Window, Line, Point
-from cell import Cell
+from window import Window
+from maze import Maze
 
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell1.draw(100, 125, 300, 400)
-    cell2 = Cell(win)
-    cell2.draw(500, 250, 10, 20)
-    cell1.draw_move(cell2, 'True')
+    maze = Maze(100, 25, 4, 10, 15, 27, win)
+    maze._create_cells()
     win.wait_for_close()
 
 
