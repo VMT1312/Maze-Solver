d6c35c26c2cea3c06aba1132d53fa11211a31e33
VMT1312 <thiemmavinh123@gmail.com>
2025-02-02 11:38:30 +0800

    Rename title to Maze Solver and update option in pack

--- window.py
+++ window.py
@@ -3,9 +3,9 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title('Title')
+        self.__root.title('Maze Solver')
         self.__canvas = Canvas(self.__root, width=width, height=height)
-        self.__canvas.pack()
+        self.__canvas.pack(fill='both', expand=1)
         self.__running = False
         self.__root.protocol('WM_DELETE_WINDOW', self.close())
 
