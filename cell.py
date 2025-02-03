0bbb64dd27ddc8d88a8a87bc36dad9e04e381845
VMT1312 <thiemmavinh123@gmail.com>
2025-02-03 17:46:35 +0800

    Change the variable location in draw method and make it more readable

--- cell.py
+++ cell.py
@@ -7,99 +7,63 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-
-        self.__x1 = None
-        self.__x2 = None
-        self.__y1 = None
-        self.__y2 = None
-        self.__win = win
         self.visited = False
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+        
 
-    def draw(self, x1, x2, y1, y2):
-        if self.__win is None:
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
             return
-        self.__x1 = x1
-        self.__x2 = x2
-        self.__y1 = y1
-        self.__y2 = y2
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
 
         if self.has_left_wall:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x1, self.__y1),
-                    Point(self.__x1, self.__y2)
-                )
-            )
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
         else:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x1, self.__y1),
-                    Point(self.__x1, self.__y2)
-                ),
-                'white'
-            )
-
-        if self.has_right_wall:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x1, self.__y1),
-                    Point(self.__x2, self.__y1)
-                )
-            )
-        else:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x1, self.__y1),
-                    Point(self.__x2, self.__y1)
-                ),
-                'white'
-            )
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, 'white')
 
         if self.has_top_wall:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x2, self.__y1),
-                    Point(self.__x2, self.__y2)
-                )
-            )
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
         else:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x2, self.__y1),
-                    Point(self.__x2, self.__y2)
-                ),
-                'white'
-            )
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, 'white')
+
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, 'white')
 
         if self.has_bottom_wall:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x1, self.__y2),
-                    Point(self.__x2, self.__y2)
-                )
-            )
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
         else:
-            self.__win.draw_line(
-                Line(
-                    Point(self.__x1, self.__y2),
-                    Point(self.__x2, self.__y2)
-                ),
-                'white'
-            )
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, 'white')
+
 
     def draw_move(self, to_cell, undo=False):
-        line = Line(
-                Point(
-                    (self.__x1 + self.__x2) / 2,
-                    (self.__y1 + self.__y2) / 2
-                ),
-                Point(
-                    (to_cell.__x1 + to_cell.__x2) / 2,
-                    (to_cell.__y1 + to_cell.__y2) / 2
-                )
-            )
-        
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
         if undo:
-            self.__win.draw_line(line, 'gray')
-        else:
-            self.__win.draw_line(line, 'red')
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
